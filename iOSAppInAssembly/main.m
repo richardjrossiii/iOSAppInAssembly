f4e37281f1f366e25e238b32cad3c2a868cf4431
RJ Ross <mrfootball@rick-ross.com>
2013-12-17 16:07:54 -0500

    Initial Commit

---
+++ iOSAppInAssembly/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOSAppInAssembly
+//
+//  Created by Angry Beast on 12/17/13.
+//  Copyright (c) 2013 richardjrossiii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
